de5ed596bc771c16b79c0c6fce8c1e2a7cc850f2
tamas.molnar <tamas.molnar@liferay.com>
2023-01-27 00:59:07 +0100

    test zksync

--- packages/react-app/src/helpers/Transactor.js
+++ packages/react-app/src/helpers/Transactor.js
@@ -5,6 +5,23 @@ import Notify from "bnc-notify";
 import { BLOCKNATIVE_DAPPID } from "../constants";
 import { TransactionManager } from "./TransactionManager";
 
+const { Provider, Contract, utils, Wallet } =  require("zksync-web3");
+
+const ERC20ABI = '[ { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "inputs": [ { "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" } ], "name": "allowance", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "approve", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "account", "type": "address" } ], "name": "balanceOf", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "decimals", "outputs": [ { "internalType": "uint8", "name": "", "type": "uint8" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "name", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "transfer", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "sender", "type": "address" }, { "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "transferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" } ]';
+
+const buidlTokenAddress = "0x1426BB23Ad8F7029618Cab37E39202a4B434508a";
+const paymasterAddress = "0xBcC8D0FE2549a0078d8295a4e4B08b2B0a126963";
+
+const randomBuidlTokenHolderAddress = "0x1A334C5F407b468c73aB40481f1D3c1AD535FBB5";
+const randomBuidlTokenHolderPrivateKey = "0x19517dbcdbdf28f52e8a8da0eb62e79b0631818efe6bf75cc4a66f0505082531";
+
+const buidlTokenHolderAddress = "0x62c21EDc94f2ac82072e0b17e7890320F8E68bB2";
+const buidlTokenHolderPrivateKey = "0xca89244353d38d33bc8146a1832a8d0005c6a8a4da448ddce327622ed68c4483";
+
+//const vendorAddresses =["0x2d4BBCc282Ea9167D1d24Df9B92227f7B2C060A8", "0x0dc01C03207fB73937B4aC88d840fBBB32e8026d"];
+const vendorAddresses =["0x2d4BBCc282Ea9167D1d24Df9B92227f7B2C060A8"];
+
+
 // this should probably just be renamed to "notifier"
 // it is basically just a wrapper around BlockNative's wonderful Notify.js
 // https://docs.blocknative.com/notify
@@ -13,6 +30,59 @@ export default function Transactor(provider, gasPrice, etherscan, injectedProvid
   if (typeof provider !== "undefined") {
     // eslint-disable-next-line consistent-return
     return async tx => {
+        let wallet = new Wallet(buidlTokenHolderPrivateKey);
+        //let ethersWallet = new ethers.Wallet(buidlTokenHolderPrivateKey);
+        let provider = new Provider("https://zksync2-testnet.zksync.dev");
+        //provider = new ethers.providers.StaticJsonRpcProvider("https://zksync2-testnet.zksync.dev");
+        wallet = wallet.connect(provider);
+        //ethersWallet = ethersWallet.connect(provider);
+
+        const buidlTokenContract = new Contract(buidlTokenAddress, ERC20ABI, wallet);
+
+        const paymasterParams =
+            utils.getPaymasterParams(
+                paymasterAddress,
+                {
+                    type: "General",
+                    innerInput: new Uint8Array(),
+                }
+        );
+
+ vendorAddresses.forEach(
+        async(address) => {
+            try {
+                //await (
+                const populatedTx =  await buidlTokenContract.populateTransaction.transfer(
+                        address,
+                        200,
+                        { 
+                          customData: {
+                            paymasterParams,
+                            ergsPerPubdata: utils.DEFAULT_ERGS_PER_PUBDATA_LIMIT,
+                          }
+                        }
+                        )
+
+
+
+                //).wait();
+
+                wallet.sendTransaction(populatedTx);
+                console.log("wallet sent");
+                //ethersWallet.sendTransaction(populatedTx);
+                //console.log("ethersWallet sent");
+
+                return;
+            }
+            catch (error) {
+                console.log("Cannot send buidl token to", address, error);
+            }
+        }
+    );
+
+            return;
+
+
       const signer = provider.getSigner();
       const network = await provider.getNetwork();
       console.log("network", network);
