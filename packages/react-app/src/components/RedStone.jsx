e9921efcf6caad48ff4530e9cb02526e477fa215
moltam89 <moltam89@gmail.com>
2023-07-05 12:15:57 +0200

    Trying to get access to the RapidExample contract on zkSyncTestnet

--- packages/react-app/src/components/RedStone.jsx
+++ packages/react-app/src/components/RedStone.jsx
@@ -1,8 +1,23 @@
 import React, { useEffect, useState } from "react";
 
+const { ethers, BigNumber, utils } = require("ethers");
+
 //import { getTokenBalance } from "../helpers/ERC20Helper";
 
+const abi = [
+    "function getLatestEthPrice() view returns (uint256)"
+];
+
+const RAPID_EXAMPLE_ADDRESS = "0x402CdcE5F1f4e85b37264EcAc1F35aCF40E609f5";
+const ZK_SYNC_TESTNET_RPC = "https://testnet.era.zksync.dev";
+
+const provider = new ethers.providers.StaticJsonRpcProvider(ZK_SYNC_TESTNET_RPC);
+
+const contract = new ethers.Contract(RAPID_EXAMPLE_ADDRESS, abi, provider);
+
 export default function RedStone({}) {
+    console.log("contract", contract);
+
     const [balance, setBalance] = useState(null);
 
     useEffect(() => {
