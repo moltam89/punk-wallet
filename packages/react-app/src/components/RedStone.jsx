d6f214713d9072a36bbb7b4b7d51d14a50ab1198
moltam89 <moltam89@gmail.com>
2023-07-05 12:34:52 +0200

    Cannot make it work

--- packages/react-app/src/components/RedStone.jsx
+++ packages/react-app/src/components/RedStone.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 
+import { Provider, Contract } from "zksync-web3";
+
 const { ethers, BigNumber, utils } = require("ethers");
 
 const { WrapperBuilder } = require("@redstone-finance/evm-connector");
-
-//import { getTokenBalance } from "../helpers/ERC20Helper";
 
 const abi = [
     "function getLatestEthPrice() view returns (uint256)"
@@ -13,9 +13,10 @@ const abi = [
 const RAPID_EXAMPLE_ADDRESS = "0x402CdcE5F1f4e85b37264EcAc1F35aCF40E609f5";
 const ZK_SYNC_TESTNET_RPC = "https://testnet.era.zksync.dev";
 
-const provider = new ethers.providers.StaticJsonRpcProvider(ZK_SYNC_TESTNET_RPC);
+//const provider = new ethers.providers.StaticJsonRpcProvider(ZK_SYNC_TESTNET_RPC);
+const provider = new Provider(ZK_SYNC_TESTNET_RPC);
 
-const contract = new ethers.Contract(RAPID_EXAMPLE_ADDRESS, abi, provider);
+const contract = new Contract(RAPID_EXAMPLE_ADDRESS, abi, provider);
 
 const wrappedContract = WrapperBuilder.wrap(contract).usingDataService(
   {
@@ -23,7 +24,7 @@ const wrappedContract = WrapperBuilder.wrap(contract).usingDataService(
     uniqueSignersCount: 1,
     dataFeeds: ["ETH"],
   },
-  //["https://d33trozg86ya9x.cloudfront.net"]
+  ["https://d33trozg86ya9x.cloudfront.net"]
 );
 
 export default function RedStone({}) {
@@ -32,11 +33,11 @@ export default function RedStone({}) {
     const [balance, setBalance] = useState(null);
 
     useEffect(() => {
-        async function getBalance() {
+        async function getETHPrice() {
             const ethPrice = await wrappedContract.getLatestEthPrice();
         }
 
-        getBalance();
+        getETHPrice();
     }, [])
 
     return (
