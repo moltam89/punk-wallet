77bd90a3a8de67173a79f7e28fdcab3e521cb064
moltam89 <moltam89@gmail.com>
2023-07-05 12:28:40 +0200

    Cannot make it work

--- packages/react-app/src/components/RedStone.jsx
+++ packages/react-app/src/components/RedStone.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 
 const { ethers, BigNumber, utils } = require("ethers");
 
+const { WrapperBuilder } = require("@redstone-finance/evm-connector");
+
 //import { getTokenBalance } from "../helpers/ERC20Helper";
 
 const abi = [
@@ -15,6 +17,15 @@ const provider = new ethers.providers.StaticJsonRpcProvider(ZK_SYNC_TESTNET_RPC)
 
 const contract = new ethers.Contract(RAPID_EXAMPLE_ADDRESS, abi, provider);
 
+const wrappedContract = WrapperBuilder.wrap(contract).usingDataService(
+  {
+    dataServiceId: "redstone-main-demo",
+    uniqueSignersCount: 1,
+    dataFeeds: ["ETH"],
+  },
+  //["https://d33trozg86ya9x.cloudfront.net"]
+);
+
 export default function RedStone({}) {
     console.log("contract", contract);
 
@@ -22,7 +33,7 @@ export default function RedStone({}) {
 
     useEffect(() => {
         async function getBalance() {
-            const ethPrice = await contract.getLatestEthPrice();
+            const ethPrice = await wrappedContract.getLatestEthPrice();
         }
 
         getBalance();
