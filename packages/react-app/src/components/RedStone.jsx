d90d40bed2ab45200d70467efff83a1945c7699d
moltam89 <moltam89@gmail.com>
2023-07-07 11:43:54 +0200

    GetOraclePrice contract

--- packages/react-app/src/components/RedStone.jsx
+++ packages/react-app/src/components/RedStone.jsx
@@ -13,6 +13,9 @@ const abi = [
 
 //const RAPID_EXAMPLE_ADDRESS = "0x402CdcE5F1f4e85b37264EcAc1F35aCF40E609f5";
 const RAPID_EXAMPLE_ADDRESS = "0x717CE2Df7fc98792852fb6d45Cd4Cb165a5D159e";
+const GET_ORACLE_PRICE_ADDRESS = "0xeD1894b58500e55e95A86fB0FCa8964Bd8799017";
+
+
 const ZK_SYNC_TESTNET_RPC = "https://testnet.era.zksync.dev";
 
 const provider = new ethers.providers.StaticJsonRpcProvider(ZK_SYNC_TESTNET_RPC);
@@ -21,6 +24,17 @@ const provider = new ethers.providers.StaticJsonRpcProvider(ZK_SYNC_TESTNET_RPC)
 const contract = new Contract(RAPID_EXAMPLE_ADDRESS, abi, provider);
 
 const wrappedContract = WrapperBuilder.wrap(contract).usingDataService(
+  {
+    dataServiceId: "redstone-rapid-demo",
+    uniqueSignersCount: 1,
+    dataFeeds: ["ETH", "USDC"],
+  },
+  ["https://d33trozg86ya9x.cloudfront.net"]
+);
+
+const oracleContract = new Contract(GET_ORACLE_PRICE_ADDRESS, abi, provider);
+
+const oracleWrappedContract = WrapperBuilder.wrap(contract).usingDataService(
   {
     dataServiceId: "redstone-rapid-demo",
     uniqueSignersCount: 1,
@@ -38,15 +52,21 @@ export default function RedStone({}) {
 
             console.log("ethPrice", ethPrice);
         }
-
         async function getUSDCPrice() {
             const USDCPrice = await wrappedContract.getLatestUSDCPrice();
 
             console.log("USDCPrice", USDCPrice);
         }
 
+        async function getOracleUSDCPrice() {
+            const USDCPrice = await oracleWrappedContract.getLatestUSDCPrice();
+
+            console.log("USDCPrice", USDCPrice);
+        }
+
         getETHPrice();
         getUSDCPrice();
+        getOracleUSDCPrice();
     }, [])
 
     return (
