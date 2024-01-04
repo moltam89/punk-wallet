e2244c9bcb40f471bc5d10803e83174a1a4d37af
moltam89 <moltam89@gmail.com>
2024-01-04 04:27:37 +0100

    Update price

--- packages/react-app/src/hooks/ExchangePrice.js
+++ packages/react-app/src/hooks/ExchangePrice.js
@@ -1,9 +1,13 @@
 import { Fetcher, Route, Token, WETH } from "@uniswap/sdk";
 import { usePoller } from "eth-hooks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function useExchangePrice(targetNetwork, mainnetProvider, pollTime) {
   const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    pollPrice();
+  }, [targetNetwork]);
 
   const pollPrice = () => {
     async function getPrice() {
