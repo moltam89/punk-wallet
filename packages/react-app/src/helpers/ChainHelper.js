90062a2fd9b2590bc7ff89bc3bd1840dafdeba1e
agocsvince <vince.agocs@gmail.com>
2024-02-21 14:29:39 +0100

    use fetched chains

--- packages/react-app/src/helpers/ChainHelper.js
+++ packages/react-app/src/helpers/ChainHelper.js
@@ -2,17 +2,12 @@ export const getBLockExplorer = (chain, name) => getBLockExplorers(chain).find(b
 
 export const getBLockExplorers = (chain) => chain.explorers.filter(explorer => isEIP3091Explorer(explorer));
 
-export const getChain = (chainId) => chains.find(chain => chain.chainId == chainId)
+export const getChain = async (chainId) => {
+   const chains = await initChains();
+   return chains.find(chain => chain.chainId == chainId);
+}
 
 const isEIP3091Explorer = (explorer) => explorer.standard == "EIP3091";
-
-// This chain list is from
-// https://github.com/ethereum-lists/
-// https://chainid.network/chains.json
-
-// ToDo: Update the list from time to time, e.g.:
-//    https://github.com/DefiLlama/chainlist/blob/8f1c353d70e9a1b00a0b6bb07391a298561ab567/utils/fetch.js#L99
-//    https://github.com/FrederikBolding/chainlist/blob/0395aa6ee3bb35c513c39afa0af9f80a3bfa4165/src/pages/index.tsx#L11
 
 const getChains = async () => {
    const response = await fetch('https://chainid.network/chains.json');
@@ -33,10 +28,3 @@ export async function initChains() {
    const chains = await getChains() || await getLocalChains();
    return chains;
 }
-
-// ONLY For testing
-let chains = [];
-initChains().then(result => {
-   chains = result
-   console.log(result)
-});
